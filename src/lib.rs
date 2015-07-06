87550fa1f48824ca0c5ed7510277423dea5536db
Simon Sapin <simon.sapin@exyr.org>
2015-07-06 18:31:56 +0200

    Reexport fmt::Format at the crate root.

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ extern crate encoding;
 extern crate test;
 
 pub use tendril::{Tendril, ByteTendril, StrTendril, SliceExt, ReadExt, SubtendrilError};
+pub use fmt::Format;
 
 pub mod fmt;
 pub mod stream;
